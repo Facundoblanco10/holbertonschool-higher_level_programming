d609970b7390722f6c259bb70133dd9e335f3661
Facundoblanco10 <facub1011@gmail.com>
2022-09-06 12:37:19 -0700

    to uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if islower(i):
+            i = chr(ord(i) - 32)
+        print(i, end='')
+    print("")
+
+
+def islower(c):
+    if ord(c) in range(97, 123):
+        return (True)
+    else:
+        return (False)
