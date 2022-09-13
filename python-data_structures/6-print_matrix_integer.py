705ea96d1cacb3008dbfed4a75f8d6efe28ca958
Facundoblanco10 <https://Facundoblanco10@github.com>
2022-09-13 10:38:07 -0300

    Matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for idx, item in enumerate(row):
-            print("{}".format(item), end='')
+            print("{:d}".format(item), end='')
             if idx != len(row) - 1:
                 print(' ', end='')
         print()
