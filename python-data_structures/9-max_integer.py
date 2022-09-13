11afc8b501b646cdb9ba86f08199387df7a8c55b
Facundoblanco10 <https://Facundoblanco10@github.com>
2022-09-13 13:19:56 -0300

    task9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+from re import M
+
+
 def max_integer(my_list=()):
-    max = my_list[0]
 
     if my_list is None or len(my_list) == 0:
         return (None)
+    max = my_list[0]
     for n in my_list:
         if max < n:
             max = n
