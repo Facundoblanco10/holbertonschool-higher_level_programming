1c60c010e11d4038023112f22dbcad420d6798f2
Facundoblanco10 <https://Facundoblanco10@github.com>
2022-09-15 16:36:33 -0300

    task12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or type(roman_string):
+    if roman_string is None or type(roman_string) != str:
         return (0)
     res_int = 0
     a_dictionary = {'I': 1, 'V': 5, 'X': 10,
