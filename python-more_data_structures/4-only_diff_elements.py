5326c1dbe642953a2a730c569206d27f21db8c7b
Facundoblanco10 <https://Facundoblanco10@github.com>
2022-09-15 10:50:25 -0300

    task4

---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    res = []
+    for elem1 in set_1:
+        for dif1 in set_2:
+            if elem1 == dif1:
+                break
+        if elem1 != dif1:
+            res.append(elem1)
+    for elem2 in set_2:
+        for dif2 in set_1:
+            if elem2 == dif2:
+                break
+        if elem2 != dif2:
+            res.append(elem2)
+    return (res)
