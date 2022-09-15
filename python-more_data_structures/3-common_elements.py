13d389f7fc19ea7db897b6513314190db8fbd846
Facundoblanco10 <https://Facundoblanco10@github.com>
2022-09-15 09:50:25 -0300

    task3

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/pyhton3
+def common_elements(set_1, set_2):
+    res = []
+    for e in set_1:
+        for el in set_2:
+            if e == el:
+                res.append(e)
+    return (res)
