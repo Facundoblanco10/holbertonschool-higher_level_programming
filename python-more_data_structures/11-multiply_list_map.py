53af6048bc264fa6557b8bf80bd12f2c394979a3
Facundoblanco10 <https://Facundoblanco10@github.com>
2022-09-15 15:38:36 -0300

    task11

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    number_list = [number] * len(my_list)
-    new_list = []
-    new_list = list(map(multiplication, my_list, number_list))
-    return (new_list)
-
-def multiplication(n, number_list):
-    return (n * number_list)
+    return (list(map(lambda a, b: a * b, my_list, [number] * len(my_list))))
