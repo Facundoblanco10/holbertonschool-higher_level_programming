e3bff16fe12079a69b5e3e5a4d86eb51e5ceadaf
Facundoblanco10 <https://Facundoblanco10@github.com>
2022-09-21 15:32:09 -0300

    task4 testing

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -4,12 +4,14 @@
 
 def print_square(size):
     """Function"""
+    if size == 0:
+        return
     if type(size) == float:
         size = int(size)
     if type(size) != int:
         raise TypeError("size must be an integer")
     if size < 0:
-        raise ValueError("size mus be >= 0")
+        raise ValueError("size must be >= 0")
     if size == 0:
         print()
     else:
