fd49c5a8616b5a9a7d02f7b871e94737df0a4c91
Facundoblanco10 <https://Facundoblanco10@github.com>
2022-09-21 20:15:10 -0300

    task6

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -2,6 +2,24 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class testing(unittest.TestCase):
+    def test_max_integer(self):
+        actual = max_integer([1, 2, 3, 4])
+        self.assertEqual(actual, 4)
+
+    def test_empty_list(self):
+        actual = max_integer([])
+        self.assertEqual(actual, '')
+
+    def test_max_integer_negative(self):
+        actual = max_integer([-1, -4, -5, -10])
+        self.assertEqual(actual, -1)
+
+    def test_only_one(self):
+        actual = max_integer([5])
+        self.assertEqual(actual, 5)
+
     def test_list(self):
-        
+        actual = max_integer(2)
+        self.assertEqual(actual, TypeError)
