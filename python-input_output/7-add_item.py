2a37b7bd49f841e9c70a59ee9e7b1a84815b0283
Facundoblanco10 <https://Facundoblanco10@github.com>
2022-09-28 13:45:33 -0300

    task7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
 """Module"""
 import sys
-import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-p_list = []
-n = len(sys.argv)
-if os.path.exists("add_item.json"):
-    load_from_json_file("add_item.json")
-
-for i in range(1, n):
-    p_list.append(sys.argv[i])
-save_to_json_file(p_list, "add_item.json")
+save_to_json_file(list(sys.argv[1:]), "add_item.json")
+load_from_json_file("add_item.json")
