5312c08f0ad6e88528caa9ad83a87bc758b5d2d3
Facundoblanco10 <https://Facundoblanco10@github.com>
2022-09-28 13:37:06 -0300

    test

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -7,5 +7,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Function"""
-    with open(filename, "w") as f:
+    with open(filename, "a") as f:
         f.write(json.dumps(my_obj))
