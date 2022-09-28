e61bd8c07a269f2c48bf6c3d0c44ff7ab66b8411
Facundoblanco10 <https://Facundoblanco10@github.com>
2022-09-28 14:35:26 -0300

    task7

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -7,5 +7,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Function"""
-    with open(filename, "a") as f:
+    with open(filename, "w") as f:
         f.write(json.dumps(my_obj))
--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,9 +2,19 @@
 """Module"""
 
 
+import os
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-save_to_json_file(list(sys.argv[1:]), "add_item.json")
-load_from_json_file("add_item.json")
+filename = "add_item.json"
+if not os.path.exists(filename):
+    save_to_json_file(sys.argv[1:], filename)
+    exit()
+try:
+    p_list = load_from_json_file(filename)
+    for i in range(1, len(sys.argv)):
+        p_list.append(sys.argv[i])
+    save_to_json_file(p_list, filename)
+except Exception:
+    Exception
