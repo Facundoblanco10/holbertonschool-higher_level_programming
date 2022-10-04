c299a6e7ae92c336ac0c9877c40bffe9b00af33d
Facundoblanco10 <5078@holbertonstudents.com>
2022-10-03 21:49:42 -0300

    task10

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,3 +9,8 @@ class Square(Rectangle):
     """Class Square"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """String method"""
+        st = f"[Square] ({self.id}) {self.x}/{self.y} - {super().height}"
+        return (st)
