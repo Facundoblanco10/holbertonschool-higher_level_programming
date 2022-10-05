7d06b48a3a29ce367d7f0504d7b05a840203fa62
Facundoblanco10 <5078@holbertonstudents.com>
2022-10-05 15:41:13 -0300

    testing

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -26,10 +26,28 @@ class testing(unittest.TestCase):
         b1 = Base([1, 2, 3])
         self.assertEqual(b1.id, [1, 2, 3])
 
-    def base_to_json_exists(self):
-        b1 = Base.to_json_string(None)
-        self.assertEqual(b1, '[]')
+    def test_base_to_json(self):
+        b1 = Base.to_json_string([{ 'id': 12 }])
+        self.assertEqual(b1, '[{"id": 12}]')
 
-    def base_to_json_exists2(self):
+    def test_base_to_json2(self):
         b1 = Base.to_json_string([])
         self.assertEqual(b1, '[]')
+
+    def test_base_to_json3(self):
+        b1 = Base.to_json_string(None)
+        self.assertEqual(b1, '[]')
+    
+    def test_base_from_json(self):
+        b1 = Base.from_json_string(None)
+        self.assertEqual(b1, [])
+
+    def test_base_from_json2(self):
+        b1 = Base.to_json_string("[]")
+        self.assertEqual(b1, '"[]"')
+    
+    def test_base_from_json3(self):
+        b1 = Base.to_json_string('[{ "id": 89 }]')
+        self.assertEqual(b1, '"[{ \\"id\\": 89 }]"')
+
+    
