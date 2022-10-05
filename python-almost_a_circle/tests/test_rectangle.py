f095002db5ff22391a73cd0e29ce87f3ef18f6c2
Facundoblanco10 <5078@holbertonstudents.com>
2022-10-05 15:53:48 -0300

    testing

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -6,12 +6,9 @@ from models.rectangle import Rectangle
 
 
 class testing(unittest.TestCase):
-    def test_rectangle(self):
-        r1 = Rectangle(3, 4)
-        self.assertEqual(str(r1), '[Rectangle] (1) 0/0 - 3/4')
-        r2 = Rectangle(3, 4, 7)
-        self.assertEqual(str(r2), '[Rectangle] (2) 7/0 - 3/4')
-        r3 = Rectangle(3, 4, 6, 9)
-        self.assertEqual(str(r3), '[Rectangle] (3) 6/9 - 3/4')
-        r4 = Rectangle(1, 4, 3, 3, 9)
-        self.assertEqual(str(r4), '[Rectangle] (9) 3/3 - 1/4')
+    def test_rectangle_errors(self):
+        self.assertRaises(TypeError, Rectangle,"1", 4)
+        self.assertRaises(TypeError, Rectangle, 1, "4")
+        self.assertRaises(TypeError, Rectangle, 1, 2, "3")
+        self.assertRaises(TypeError, Rectangle, 1, 2, 3, "4")
+        self.assertRaises(ValueError, Rectangle, -1, 4)
