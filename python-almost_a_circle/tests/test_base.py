fae3a5b7f04c8a0631996687682bef6044335458
Facundoblanco10 <5078@holbertonstudents.com>
2022-10-05 13:01:19 -0300

    testig

---
+++ python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Test Base"""
+import unittest
+
+from models.base import Base
+
+
+class testing(unittest.TestCase):
+    def test_create_without_id(self):
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+
+    def test_create_with_id(self):
+        b1 = Base(12)
+        self.assertEqual(b1.id, 12)
+
+    def test_negative_number(self):
+        b1 = Base(-5)
+        self.assertEqual(b1.id, -5)
+
+    def test_string(self):
+        b1 = Base("hola")
+        self.assertEqual(b1.id, "hola")
+
+    def test_list(self):
+        b1 = Base([1, 2, 3])
+        self.assertEqual(b1.id, [1, 2, 3])
