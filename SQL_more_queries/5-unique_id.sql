f3b9adb9177073b6f7be5a794dc3ad34e6b67e10
FacuPS <5078@holbertonstudents.com>
2022-11-06 14:52:04 -0800

    task5

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,3 @@
 -- script that creates the table unique_id on your MySQL server.
 -- task5
-CREATE TABLE IF NOT EXISTS unique_id (id INT NOT NULL UNIQUE, name VARCHAR(256));
-INSERT INTO unique_id (id)
-VALUES (1)
+CREATE TABLE IF NOT EXISTS unique_id (id INT default 1 UNIQUE, name VARCHAR(256));
