8b6a3374755318bcdd04cd75c296db8c70d3505b
FacuPS <5078@holbertonstudents.com>
2022-11-07 19:41:42 -0800

    task3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     cn = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3])
-    
+
     cur = cn.cursor()
     cur.execute("SELECT id, name FROM states"
                 " WHERE BINARY name = %s"
