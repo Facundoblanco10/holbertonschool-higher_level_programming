d712e7c2009bc87b1f59e9acf7a5b74c76a02d75
Facub10 <facub1011@gmail.com>
2022-11-10 02:20:35 -0300

    fix task13

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
     session = Session()
     state = session.query(State).order_by(State.id).first()
     for state in session.query(State).order_by(State.id):
-        if state.name.find('a') == -1:
+        if state.name.find('a') != -1:
             session.delete(state)
             session.commit()
