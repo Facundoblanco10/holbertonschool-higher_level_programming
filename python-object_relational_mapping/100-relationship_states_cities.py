909ee165729c14a7c50026ffc0672288d92fdc86
Facub10 <facub1011@gmail.com>
2022-11-17 20:44:15 -0300

    task100

---
+++ python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+   script that creates the State “California” with the City “San Francisco”
+   from the database hbtn_0e_100_usa: (100-relationship_states_cities.py)
+"""
+if __name__ == "__main__":
+    import sys
+    from relationship_state import Base, State
+    from relationship_city import City
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
+
+    engine = create_engine('mysql://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = State(name='California')
+    city = City(name='San Francisco')
+    state.cities.append(city)
+    session.add(state)
+    session.commit()
