06cfdfb9acb70b9e7e888dc63adc45ae46bda3c5
Facub10 <facub1011@gmail.com>
2022-11-17 20:52:55 -0300

    fix

--- python-object_relational_mapping/100-relationship_states_cities.py
+++ python-object_relational_mapping/100-relationship_states_cities.py
@@ -7,13 +7,13 @@ if __name__ == "__main__":
     import sys
     from relationship_state import Base, State
     from relationship_city import City
-    from sqlalchemy.orm import Session
+    from sqlalchemy.orm import sessionmaker
     from sqlalchemy import create_engine
 
     engine = create_engine('mysql://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
-    session = Session(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
     state = State(name='California')
     city = City(name='San Francisco')
     state.cities.append(city)
