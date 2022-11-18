74fd2093271a9eab37195a4cceef5c3adea74b46
Facub10 <facub1011@gmail.com>
2022-11-18 14:46:46 -0300

    Fix

--- python-object_relational_mapping/relationship_city.py
+++ python-object_relational_mapping/relationship_city.py
@@ -5,6 +5,7 @@ City and an instance Base = declarative_base()
 """
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from relationship_state import State, Base
 
 Base = declarative_base()
 
