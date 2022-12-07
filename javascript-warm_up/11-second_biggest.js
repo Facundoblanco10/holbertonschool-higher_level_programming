1b6eb9dadc6d72aa4117caadf9be083db6808b17
Facub10 <facub1011@gmail.com>
2022-12-07 16:27:25 -0300

    Task 11 fix

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 
-let args = require('process').argv;
-
-if (args[2] && args[3]){
+const args = require('process').argv;
+let i = 0;
+if (args[2] && args[3]) {
   for (i in args) {
     args[i] = parseInt(args[i]);
   }
-  console.log(args.sort(function(a, b){return b - a})[3]);
+  console.log(args.sort(function (a, b) { return b - a; })[3]);
+} else {
+  console.log(0);
 }
-else {
-  console.log(0)
-}
-console.log(args.sort(function(a, b){return b - a;}));
