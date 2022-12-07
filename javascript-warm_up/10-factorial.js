0972392aebd28f4a4356d6e840f5a77e688dd18c
Facub10 <facub1011@gmail.com>
2022-12-07 15:31:50 -0300

    Task 10 factorial

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const args = require('process').argv;
+const num = parseInt(args[2]);
+console.log(factorialize(num));
+function factorialize (num) {
+  if (num < 0) {
+    return -1;
+  } else if (num === 0 || !num) {
+    return 1;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    return num * factorialize(num - 1);
+  }
+}
