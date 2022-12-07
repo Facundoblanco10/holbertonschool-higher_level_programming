965be8883840e310f455b9429df469abf1911aa9
Facub10 <facub1011@gmail.com>
2022-12-07 14:58:15 -0300

    Task 8 fix

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -2,6 +2,10 @@
 
 const args = require('process');
 const size = parseInt(args.argv[2]);
+if (!size) {
+    console.log('Missing size');
+    process.exit(1);
+}
 let i = 0;
 while (i < size) {
   console.log('X'.repeat(size));
