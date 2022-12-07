ea732793b130d67e9bac68e59e75c7ecb03755f9
Facub10 <facub1011@gmail.com>
2022-12-07 14:59:08 -0300

    Task 8 fix semistandard

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,8 +3,8 @@
 const args = require('process');
 const size = parseInt(args.argv[2]);
 if (!size) {
-    console.log('Missing size');
-    process.exit(1);
+  console.log('Missing size');
+  process.exit(1);
 }
 let i = 0;
 while (i < size) {
