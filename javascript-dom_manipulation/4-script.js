c1fb0fd4d8f5abc60c95690c2732d8b0b4693624
Facub10 <facub1011@gmail.com>
2022-12-15 23:54:38 -0300

    Task 4 done

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const addItemButton = document.getElementById('add_item');
+const myList = document.querySelector('.my_list');
+
+addItemButton.addEventListener('click', function() {
+    const newListItem = document.createElement('li');
+    newListItem.innerHTML = 'Item';
+    myList.appendChild(newListItem);
+  });
